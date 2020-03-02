4b46ee1961664b9a11ef065f54bfc19edbd8757d
Pendramon <Pendramon@users.noreply.github.com>
2020-03-02 17:08:45 +0100

    Added basic testing.

--- c1/index.js
+++ c1/index.js
@@ -1,13 +1,46 @@
-
-function tConvert(temperature, conversionType) {
+const tConvert = (temperature, conversionType) => {
     switch (conversionType) {
         case "C2F":
-            return ((temperature * 1.8) + 32);
+            return (temperature * 1.8) + 32;
         case "F2C":
-            return ((temperature - 32) / 1.8);
+            return (temperature - 32) / 1.8;
         default:
             return null;
     }
 }
 
 console.log(tConvert(132, "F2C"));
+
+const logFails = true;
+let allHasPassed = true;
+
+const TestFail = (nameOfTest, expected, result) => {
+    if (logFails == true) {
+        console.log("WARN: " + nameOfTest + " failed; Expected: " + expected + "; Got: " + result + ";");
+    }
+    allHasPassed = false;
+}
+
+(function ShouldConvertFarenheitToCelsius() {
+    // Arange
+    const expected = 55.55555555555556;
+
+    // Act
+    const result = tConvert(132, "F2C");
+
+    // Assert
+    result == expected ? null : TestFail("Test 1", expected, result);
+})();
+
+(function ShouldConvertCelsiusToFarenheit() {
+    // Arange
+    const expected = 89.6;
+
+    // Act
+    const result = tConvert(32, "C2F");
+
+    // Assert
+    result == expected ? null : TestFail("Test 2", expected, result);
+})();
+
+allHasPassed ? console.log("All tests have passed.") : console.log("Tests failed.")
